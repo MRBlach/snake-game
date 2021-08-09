0a595d3429336fa17edb468479b23ff4c45b679e
Melody Blachowicz <melodyblachowicz@gmail.com>
2021-08-08 17:41:20 -0700

    Update food.js

--- food.js
+++ food.js
@@ -1,10 +1,12 @@
+// Import data from the snake.js file; seperate each import with a comma and include the from file path
+import { onSnake, expandSnake } from './snake.js';
+import { randomGridPosition } from './grid.js';
 
-import { onSnake, expandSnake } from './snake.js'
-import { randomGridPosition } from './grid.js'
-
+// Assign position of food to a variable
 let food = getRandomFoodPosition()
+// Declare added length of snake upon food consumption
 const EXPANSION_RATE = 2
-
+// Update snake length if food eaten
 export function update() {
  if (onSnake(food)) {
    expandSnake(EXPANSION_RATE)
@@ -12,19 +14,21 @@ export function update() {
  }
 }
 
-// render the gameboard 
+// Render the gameboard 
 export function draw(gameBoard) {
-    
+    // Create food element
     const foodElement = document.createElement('div')
     foodElement.style.gridRowStart = food.y
     foodElement.style.gridColumnStart = food.x
     foodElement.classList.add('food')
-
+  
+  // Append food element to the gameboard   
   gameBoard.appendChild(foodElement)
   
   
 }
 
+// Move food to new random space after snake eats it
 function getRandomFoodPosition() {
   let newFoodPosition
   while (newFoodPosition == null || onSnake(newFoodPosition)) {
